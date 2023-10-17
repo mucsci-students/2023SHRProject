af15cec4311fd6e6e7ffa63305c16f9a7c64346d
Mitchell Harrison <mitchellharrison42@gmail.com>
2023-10-17 18:20:48 -0400

    All changes reverted from previous 4 commits done by mitchell Harrison

--- Assets/Code/Scripts/TowerScripts/BoomerangMonkeyScript.cs
+++ Assets/Code/Scripts/TowerScripts/BoomerangMonkeyScript.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class BoomerangMonkeyScript : MonkeyScript
 {
-    //protected override void Fire(GameObject target)
-    //{
-        //TODO: implement boomerang logic, I was thinking a parabolic function? Or a script to handle this logic 
-        //or we could use the distance traveled as an indicator for when to return to the monkey?
-    //}
-    
-    
     protected override void Upgrade1_1()
     {
         //Multi-Target 
