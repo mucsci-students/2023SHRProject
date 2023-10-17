8b5f297c67685baa73997e144b166ad177c5607e
jonathan <jjriver1@millersville.edu>
2023-10-17 17:04:15 -0400

    changed some elements in main menu

---
+++ Assets/Code/Scripts/UI/BGScroller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGScroller : MonoBehaviour
+{
+    [SerializeField] private RawImage img;
+    [SerializeField] private float x, y;
+    void Update()
+    {
+        img.uvRect = new Rect(img.uvRect.position + new Vector2(x, y) * Time.deltaTime, img.uvRect.size);
+    }
+}
