d6b65df22044c7571553d1bad743412017033dcc
jonathan <jjriver1@millersville.edu>
2023-10-22 00:26:20 -0400

    set up SpikeFactory - no functionality

---
+++ Assets/Code/Scripts/TowerScripts/SpikeFactoryScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeFactoryScript : MonkeyScript
+{
+    protected override void Upgrade1_1()
+    {   
+        //Bigger Stacks 
+        //Description: Creates larger piles of spikes per shot.
+        //Effect: Spike piles gain +5 pierce. For Perma-Spike, gains +40 pierce.
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Upgrade1_2()
+    {
+        //White Hot Spikes
+        //Description: Cuts through Lead Bloons and Frozen Bloons.
+        //Effect: Can pop Frozen and Lead Bloons.
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Upgrade2_1()
+    {
+        //Faster Production 
+        //Description: Rapidly increases production rate.
+        //Effect: Produces spikes +25% faster (0.80x attack cooldown)
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Upgrade2_2()
+    {
+        //Even Faster Production 
+        //Description: Increases production rate even more.
+        //Effect: Produces spikes +42% faster (0.70x attack cooldown)
+        throw new System.NotImplementedException();
+    }
+}
