3059e7ae7fff292c94524e27c80b268ca5b0cf8d
jonathan <jjriver1@millersville.edu>
2023-10-30 21:52:36 -0400

    tweaked dartGunner

--- Assets/Code/Scripts/TowerScripts/DartlingGunnerScript.cs
+++ Assets/Code/Scripts/TowerScripts/DartlingGunnerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
 public class DartlingGunnerScript : MonkeyScript
@@ -14,7 +15,7 @@ public class DartlingGunnerScript : MonkeyScript
 
     // Spread angle in degrees
     [SerializeField]
-    private float spreadAngle = 10.0f;
+    private float spreadAngle = 6.0f;
 
     protected override void Fire(GameObject target)
     {
@@ -49,38 +50,34 @@ public class DartlingGunnerScript : MonkeyScript
     {
         //Focused Firing COST: $255
         //Description: Increases accuracy of the Dartling Gun.
-        //Effect: Decreases angular deviation from 23° to 9.6°,
-        //reducing projectile deviation by 60%. Also increases
-        //projectile lifespan for all Buckshot projectiles by +40%.
-        throw new System.NotImplementedException();
+        //Effect: Decreases angular deviation
+        spreadAngle = 2.5f;
     }
 
     protected override void Upgrade1_2()
     {
-        //Powerful Darts COST: $1,020
+        //Powerful Darts COST: $510
         //Description: Darts move faster and can pop 3 bloons each.
-        //Effect: Darts gain +2 pierce per shot, pop Frozen, and increases
-        //projectile speed and projectile size by 50%. For Hydra Rocket Pods,
-        //gains +2 pierce and one more explosion plus extra blast radius, most other
-        //Tier 3 crosspaths and above receive +50% pierce overall.
-        throw new System.NotImplementedException();
+        projectileSpeed = 28;
+        layersPoppedPerHit = 3;
     }
 
     protected override void Upgrade2_1()
     {
-        //Faster Barrel Spin COST: $810
+        //Faster Barrel Spin COST: $215
         //Description: Makes gun fire much faster.
-        //Effect: Attacks +50% faster (0.66x attack cooldown).
-        throw new System.NotImplementedException();
+        //Effect: Attacks +?% faster
+        firingRate = 0.45f;
     }
 
     protected override void Upgrade2_2()
     {
-        //Hydra Rocket Pods COST: $4,080
-        //Description: Shoots depleted bloontonium missiles that can damage all
-        //Bloon types and trigger multiple explosions.
-        //Effect: Hydra Rocket Pod missiles damage any bloon type and produce up to
-        //3 low-pierce explosions by default. Gains even more explosions with pierce buffs.
-        throw new System.NotImplementedException();
+        //Buckshot COST: $1020
+        //Description: Shoots blasts of deadly buckshot instead of darts.
+        //more darts, more accuracy, more speed
+        //change projectile prefab??
+        projectilesPerBurst = 4;
+        burstDelay = 0.01f;
+        firingRate = 0.6f;
     }
 }
