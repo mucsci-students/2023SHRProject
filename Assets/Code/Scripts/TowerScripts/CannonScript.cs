2ab3fe82f74aee96c8dae495bd9bf43d800b6a01
Mitchell Harrison <mitchellharrison42@gmail.com>
2023-11-08 21:21:20 -0500

    Added cannon and upgrades

--- Assets/Code/Scripts/TowerScripts/CannonScript.cs
+++ Assets/Code/Scripts/TowerScripts/CannonScript.cs
@@ -7,26 +7,26 @@ public class CannonScript : MonkeyScript
 {
     protected override void Upgrade1_1()
     {
-        //Long Range Darts
+        //Increase range+
         radiusSpriteRenderer.transform.localScale *= 1.1f;
     }
 
     protected override void Upgrade1_2()
     {
-        //Enhanced Eyesight
+        //Increase range++
         radiusSpriteRenderer.transform.localScale *= 1.11f;
         //TODO: ADD CAMO
     }
 
     protected override void Upgrade2_1()
     {
-        //Sharp Shots
-        layersPoppedPerHit = 2;
+        //Fast Speed
+        projectileSpeed = 2;
     }
 
     protected override void Upgrade2_2()
     {
-        //Razor Sharp Shots
-        layersPoppedPerHit = 4;
+        //Fastest Speed
+        projectileSpeed = 3;
     }
 }
