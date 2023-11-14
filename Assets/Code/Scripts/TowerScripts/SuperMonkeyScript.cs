1873001596d64527e09f8d51f0e5174539b5276c
Justin <jmms5757@gmail.com>
2023-11-13 19:51:30 -0500

    added super monkey

---
+++ Assets/Code/Scripts/TowerScripts/SuperMonkeyScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperMonkeyScript : MonkeyScript
+{
+    /// <inheritdoc />
+    protected override void Upgrade1_1()
+    {
+        radiusSpriteRenderer.transform.localScale *= 1.1f;
+    }
+
+    /// <inheritdoc />
+    protected override void Upgrade1_2()
+    {
+        radiusSpriteRenderer.transform.localScale *= 1.1f;
+    }
+
+    /// <inheritdoc />
+    protected override void Upgrade2_1()
+    {
+        firingRate = 0.15f;
+    }
+
+    /// <inheritdoc />
+    protected override void Upgrade2_2()
+    {
+        firingRate = 0.10f;
+    }
+}
