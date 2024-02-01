d5c8c8f1e96ccae06f7d61c939ee37a26c606bd5
jonathan <jjriver1@millersville.edu>
2024-01-31 21:18:03 -0500

    stuf

---
+++ Assets/Code/Scripts/AI/HardCodedAI/Goal.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal: MonoBehaviour
+{
+    //things to store
+        //store monkey script type
+        //
+    private enum Decision
+    {
+        DoNothing,
+        PlaceTower,
+        //UpgradeTower
+    }
+    
+    
+    
+    //getNewGoal
+    
+    //setNewGoal
+    
+    //canAchieveGoal
+    
+    //executeGoal
+}
